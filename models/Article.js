0199f6e325d0387984922b9d94d8d65c55feae9b
Matt Wilson <matt@firstflightagency.com>
2019-12-15 18:06:58 -0500

    Added Models for db

---
+++ models/Article.js
@@ -0,0 +1,36 @@
+var mongoose = require("mongoose");
+var Schem = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: String,
+        default: "Summary Unavailable"
+    },
+    isSaved: {
+        type: Boolean,
+        default: false
+    },
+    status: {
+        type: String,
+        default: "Save Article"
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }
+})
+
+var Article = mongoose.model("Article", ArticleSchema);
+module.exports = Article;
